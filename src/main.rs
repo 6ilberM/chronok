f944d9bbc05522e6a28c874a070b8412e28bd0d2
Gilber Moreno <6ilberm@gmail.com>
2023-11-26 21:12:02 -0500

    bump to version 0.1.1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use chrono::{Local, Timelike, Datelike, Weekday};
-use crossterm::{execute, queue, style::{self, Stylize}, cursor, terminal, event::{self, Event, KeyCode}};
-use std::io::{self, Write};
+use chrono::{Local, Timelike, Datelike, TimeZone, DateTime};
+use crossterm::{execute, terminal, event::{self, Event, KeyCode}};
+use std::io::{self};
 use serde::Deserialize;
 use std::time::Duration;
-use std::thread;
+use colored::Colorize;
 
 #[derive(Deserialize)]
 struct Config {
     progress_char: String,
+    refresh_rate_in_millis: u64,
 }
 
 struct ProgressBar {
@@ -26,10 +27,9 @@ impl ProgressBar {
     }
 }
 
-fn queue_text(stdout: &mut std::io::Stdout, x: u16, y: u16, text: &str) -> Result<(), Box<dyn std::error::Error>> {
-    queue!(stdout, cursor::MoveTo(x, y))?;
-    queue!(stdout, style::PrintStyledContent(text.white().on_black()))?;
-    Ok(())
+fn queue_text(buffer: &mut String, x: u16, y: u16, text: &str, color: colored::Color) {
+    buffer.push_str(&format!("\x1B[{};{}H", y + 1, x + 1)); // Move cursor to position
+    buffer.push_str(&format!("{}", text.color(color))); // Add colored text
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -41,15 +41,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Enable non-blocking reads
     execute!(stdout, event::EnableMouseCapture)?;
     // Get initial terminal size
-    let (_, terminal_height) = terminal::size()?;
+    let (_, _terminal_height) = terminal::size()?;
     let text_x = 0;
-    let text_y = terminal_height / 2;
+    let position = crossterm::cursor::position()?;
+    let text_y = position.1 as u16 + 1;
+
+    let mut buffer = String::new(); // Buffer to hold terminal content
 
     loop {
+        buffer.clear(); // Clear the buffer at the start of each loop iteration
+
         let now = Local::now();
         let weekday = now.weekday();
         let total_minutes_day = 24 * 60;
         let current_minutes_day = now.hour() * 60 + now.minute();
+        let start_of_year = get_start_of_year(now);
+        let duration_since_start_of_year = now.signed_duration_since(start_of_year);
+        let current_week = (now - start_of_year).num_weeks() + 1;
         let percentage_day = (current_minutes_day as f32 / total_minutes_day as f32) * 100.0;
         let day_progress_bar = ProgressBar::new(config.progress_char.clone(), percentage_day);
         let day_process_text = format!(
@@ -66,20 +74,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let week_progress_bar = ProgressBar::new(config.progress_char.clone(), percentage_week);
         let week_process_text = format!(
             "Week Process: [W:{:02}][{:02.0}%][{}]",
-            now.iso_week().week(),
+            current_week,
             percentage_week,
             week_progress_bar.render()
+        );
+
+        // Year progress
+        let total_minutes_year = 365 * total_minutes_day; // assuming non-leap year for simplicity
+        let current_minutes_year = duration_since_start_of_year.num_minutes();
+        let percentage_year = (current_minutes_year as f32 / total_minutes_year as f32) * 100.0;
+        let year_progress_bar = ProgressBar::new(config.progress_char.clone(), percentage_year);
+        let year_process_text = format!(
+            "Year Process: [Y:{:04}][{:02.0}%][{}]",
+            now.year(),
+            percentage_year,
+            year_progress_bar.render()
         );
 
         let time_text = format!("TIME: {:02}:{:02}:{:02}", now.hour(), now.minute(), now.second());
         let date_text = format!("DATE: {:02}/{:02}/{:02}", now.day(), now.month(), now.year() % 100);
 
-        queue_text(&mut stdout, text_x, text_y, &time_text)?;
-        queue_text(&mut stdout, text_x, text_y + 2, &date_text)?;
-        queue_text(&mut stdout, text_x, text_y + 4, &day_process_text)?;
-        queue_text(&mut stdout, text_x, text_y + 6, &week_process_text)?;
+        // Replace all queue_text calls with the updated function
+        queue_text(&mut buffer, text_x, text_y, &time_text, colored::Color::Red);
+        queue_text(&mut buffer, text_x, text_y + 1, &date_text, colored::Color::Blue);
+        queue_text(&mut buffer, text_x, text_y + 2, &day_process_text, colored::Color::Green);
+        queue_text(&mut buffer, text_x, text_y + 3, &week_process_text, colored::Color::Yellow);
+        queue_text(&mut buffer, text_x, text_y + 4, &year_process_text, colored::Color::Magenta);
 
-        stdout.flush()?;
+        // Print the buffer content
+        print!("{}", buffer);
 
         if event::poll(Duration::from_secs(0))? {
             if let Ok(Event::Key(event)) = event::read() {
@@ -91,8 +114,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
 
-        thread::sleep(Duration::from_millis(100));
+        std::thread::sleep(std::time::Duration::from_millis(config.refresh_rate_in_millis));
     }
 
     Ok(())
+}
+
+fn get_start_of_year(now: DateTime<Local>) -> DateTime<Local> {
+    let start_of_year = Local.with_ymd_and_hms(now.year(), 1, 1, 0, 0, 0).unwrap();
+    start_of_year
 }
